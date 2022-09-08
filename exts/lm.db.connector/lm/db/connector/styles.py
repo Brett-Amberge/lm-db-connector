1e8dd63ff272b34779e29b491936ff74c4ebd04c
Brett-Amberge <brett.j.amberge@lmco.com>
2022-09-08 09:11:10 -0400

    Formatting

--- exts/lm.db.connector/lm/db/connector/styles.py
+++ exts/lm.db.connector/lm/db/connector/styles.py
@@ -1,6 +1,4 @@
-"""
-    Defines some re-usable UI styles
-"""
+""" Defines some re-usable UI styles """
 
 import omni.ui as ui
 
@@ -9,17 +7,17 @@ rect_style = {
     "border_color": 0xFF000000,
     "border_width": 1,
     "margin": 0
-    }
+}
 
 lab_style = {
     "alignment": ui.Alignment.CENTER,
     "color": 0xFF00B976
-    }
+}
 
 btn_style = {
     "alignment": ui.Alignment.CENTER,
     "color": 0xFF00B976
-    }
+}
 
 db_style = {
     "alignment": ui.Alignment.RIGHT,
