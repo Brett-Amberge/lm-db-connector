84a25e1f37bbbdecf5b8336ea9ce994ff44908e3
Brett-Amberge <brett.j.amberge@lmco.com>
2022-09-12 11:24:48 -0400

    Moved DB and UI functions to manipulator for hot reloading when an object is selected

--- exts/lm.db.connector/lm/db/connector/extension.py
+++ exts/lm.db.connector/lm/db/connector/extension.py
@@ -5,17 +5,7 @@ import omni.ui as ui
 import carb
 from omni.kit.viewport.utility import get_active_viewport_window
 
-from .db_model import DBModel
 from .viewport_scene import ViewportScene
-from .styles import lab_style, rect_style, btn_style, db_style
-
-# Try to import the mysql library
-try:
-    import pymysql
-except ImportError:
-    carb.log_warn("[lm.db.connector] PyMysql not found, attempting to install")
-    omni.kit.pipapi.install("pymysql")
-    import pymysql
 
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
@@ -25,90 +15,13 @@ class Connector(omni.ext.IExt):
 
     # Constructor
     def __init__(self):
-        self._model = DBModel()
-        self._window = None
-        self._cnx = None
-        self._connected = False
-
         self._viewport_scene = None
-
-    # Attempt to connect to the sql database with user provided username and password
-    def connect(self, user, password):
-        try:
-            self._cnx = pymysql.connect(user=user, password=password, host='127.0.0.1', port=3306, database='sakila')
-            self._connected = True
-        except:
-            # Alert the user if the connection fails
-            carb.log_warn("Failed to connect to database")
-            return False
-
-    # Update the model when the text field is changed
-    def on_changed(self, item, value):
-        self._model.set_value(item, value)
-
-    # Populate the UI with any information retrieved from the database
-    def on_pressed(self):
-        self._model.set_value(self._model.get_item("result"),
-                            self.connect(self._model.get_value(self._model.get_item("user")),
-                                        self._model.get_value(self._model.get_item("pass"))))
-        self._model.set_value(self._model.get_item("result"), self.query())
-        self.build_ui()
-
-    def query(self, query=False):
-        if self._connected:
-            with self._cnx:
-                with self._cnx.cursor() as cursor:
-                    sql = query if query else "SELECT * FROM film"
-                    cursor.execute(sql)
-                    result = cursor.fetchall()
-                    return result
-        else:
-            carb.log_warn("[lm.db.connector] Connection error")
-
-    # Build the window's UI based on information from the model
-    def build_ui(self):
-        with self._window.frame:
-            if not self._connected:
-                with ui.VStack():
-                    with ui.HStack(height=0):
-                        with ui.VStack():
-                            ui.Label("Username:", style=lab_style)
-                            uField = ui.StringField()
-                            uField.model.add_end_edit_fn(lambda m,
-                                                        item=self._model.get_item("user"):
-                                                        self.on_changed(item, m.get_value_as_string()))
-
-                        with ui.VStack():
-                            ui.Label("Password:", style=lab_style)
-                            pField = ui.StringField(password_mode=True)
-                            pField.model.add_end_edit_fn(lambda m,
-                                                        item=self._model.get_item("pass"):
-                                                        self.on_changed(item, m.get_value_as_string()))
-
-                    ui.Button(text="Connect to a database", clicked_fn=lambda: self.on_pressed(),
-                              style=btn_style)
-
-            else:
-                with ui.VStack():
-                    ui.Label("Query Result:", style=lab_style, height=0)
-
-                    result = self._model.get_value(self._model.get_item("result"))
-                    with ui.ScrollingFrame(horizontal_scrollbar_policy=ui.ScrollBarPolicy.SCROLLBAR_AS_NEEDED,
-                                        vectrical_scrollbar_policy=ui.ScrollBarPolicy.SCROLLBAR_AS_NEEDED):
-                        with ui.VGrid():
-                            for entry in result:
-                                with ui.HStack():
-                                    for i in range(len(entry)):
-                                        with ui.ZStack():
-                                            ui.Rectangle(style=rect_style)
-                                            ui.Label(str(entry[i]), style=db_style)
 
     # ext_id is current extension id. It can be used with extension manager to query additional information, like where
     # this extension is located on filesystem.
     def on_startup(self, ext_id):
 
         print("[lm.db.connector] MyExtension startup")
-        self._window = ui.Window("Database Info", width=500, height=300)
 
         # Get the active Viewport
         viewport_window = get_active_viewport_window()
@@ -119,9 +32,6 @@ class Connector(omni.ext.IExt):
 
         # Build out the scene
         self._viewport_scene = ViewportScene(viewport_window, ext_id)
-
-        # Build out the UI
-        self.build_ui()
 
     def on_shutdown(self):
         print("[lm.db.connector] MyExtension shutdown")
--- exts/lm.db.connector/lm/db/connector/scene_selector.py
+++ exts/lm.db.connector/lm/db/connector/scene_selector.py
@@ -5,9 +5,28 @@
 from omni.ui import scene as sc
 from omni.ui import color as cl
 import omni.ui as ui
+import omni.kit
+import carb
+
+# Attempt to import the mysql package
+try:
+    import pymysql
+except ImportError:
+    carb.log_warn("[lm.db.connector] pymysql not found, attempting to install")
+    omni.kit.pipapi.install("pymysql")
+    import pymysql
 
 
 class SceneSelector(sc.Manipulator):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._cnx = None
+        self._connected = False
+        self._window = ui.Window("Database UI", width=200, height=200)
+
+        self.build_ui()
 
     # Rebuild when the model is changed
     def on_build(self):
@@ -17,6 +36,13 @@ class SceneSelector(sc.Manipulator):
         if self.model.get_item("name") == "":
             return
 
+        self.connect()
+
+        query = "SELECT meta FROM omnitest WHERE pth=" + "\"" + self.model.get_item('name') + "\""
+        res = self.query(query)
+        carb.log_warn(res[0])
+        self.model.set_value(self.model.get_item("result"), res)
+
         position = self.model.get_value(self.model.get_item("position"))
 
         # Move everything to where the selected object is
@@ -25,6 +51,34 @@ class SceneSelector(sc.Manipulator):
             with sc.Transform(look_at=sc.Transform.LookAt.CAMERA):
                 sc.Label(f"Prim: {self.model.get_item('name')}", alignment=ui.Alignment.LEFT_BOTTOM)
 
+        self.build_ui()
+
     # Regenerates the manipulator
     def on_model_updated(self, item):
         self.invalidate()
+
+    # Attempt to connect to the sql database with user provided username and password
+    def connect(self):
+        try:
+            self._cnx = pymysql.connect(user='root', password='', host='127.0.0.1', port=3306,
+                                        database='sakila')
+            self._connected = True
+        except:
+            # Alert the user if the connection fails
+            carb.log_warn("Failed to connect to database")
+            return False
+
+    def query(self, query=False):
+        if self._connected:
+            with self._cnx:
+                with self._cnx.cursor() as cursor:
+                    cursor.execute(query)
+                    result = cursor.fetchone()
+                    return result
+        else:
+            carb.log_warn("[lm.db.connector] Connection error")
+
+    # Build UI elements based on data stored in model
+    def build_ui(self):
+        with self._window.frame:
+            ui.Label(f"Query result: {self.model.get_value(self.model.get_item('result'))}")
